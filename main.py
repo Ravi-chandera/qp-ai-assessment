9c8731337085cba84304afb5d1c4bb8888477c66
Ravi-chandera <110690059+Ravi-chandera@users.noreply.github.com>
2024-02-12 19:57:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,27 +9,8 @@ from langchain.vectorstores import FAISS
 from langchain.llms import CTransformers
 from langchain.chains import RetrievalQA
 
-DATA_PATH = 'data/'
-DB_FAISS_PATH = 'vectorstore/db_faiss'
 
-# Create vector database
-def create_vector_db():
-    loader = DirectoryLoader(DATA_PATH,
-                             glob='*.pdf',
-                             loader_cls=PyPDFLoader)
 
-    documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,
-                                                   chunk_overlap=50)
-    texts = text_splitter.split_documents(documents)
-
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
-                                       model_kwargs={'device': 'cuda'})
-
-    db = FAISS.from_documents(texts, embeddings)
-    db.save_local(DB_FAISS_PATH)
-
-create_vector_db()
 
 DB_FAISS_PATH = 'vectorstore/db_faiss'
 
